1f7057207aa1ec73e7d4c50212d23c0f73a39d1c
megabyte512 <shlolfwall@gmail.com>
2026-02-14 14:24:32 -0700

    progress on diamond tennis racket

--- src/main/java/com/megabyte512/tennis/TennisMod.java
+++ src/main/java/com/megabyte512/tennis/TennisMod.java
@@ -6,6 +6,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,17 +15,26 @@ import org.slf4j.LoggerFactory;
 public class TennisMod implements ModInitializer {
 	public static final String MOD_ID = "tennis";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	public static final Item DIAMOND_RACKET = new Item(new Item.Settings().maxCount(1));
+	
+	public static Item DIAMOND_RACKET;
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "diamond_racket"), DIAMOND_RACKET);
+		// Create identifier and registry key
+		Identifier id = Identifier.of(MOD_ID, "diamond_racket");
+		RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
 		
+		// Create item with registry key in settings
+		Item.Settings settings = new Item.Settings().maxCount(1).registryKey(key);
+		
+		// Register the item
+		DIAMOND_RACKET = Registry.register(Registries.ITEM, key, new Item(settings));
+		
+		// Add to creative tab
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
 			content.add(DIAMOND_RACKET);
 		});
 		
-		LOGGER.info("Tennis mod initialized");
+		LOGGER.info("Tennis mod initialized!");
 	}
 }
